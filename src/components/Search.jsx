08d92fa44070d5df804e5bc9b17f68c047d601bb
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-08-01 19:06:59 -0700

    Update every component to ES6

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,27 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+// var Search = () => (
+//   <div className="search-bar form-inline">
+//     <input className="form-control" type="text" />
+//     <button className="btn hidden-sm-down">
+//       <span className="glyphicon glyphicon-search"></span>
+//     </button>
+//   </div>
+// );
+
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import exampleVideoData from './data/exampleVideoData.js';
+import Search from './components/Search.js';
+
+// console.log(exampleVideoData);
+
+ReactDOM.render(<App />, document.getElementById('app')); // this is where we render the app
+
 
