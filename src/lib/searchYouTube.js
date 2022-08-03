e8744f98965d28940278a696bcdbcf0b7e715d48
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-08-02 17:31:00 -0700

    update the ajax

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -5,7 +5,62 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
 });
 
 var searchYouTube = (query, callback) => {
-  // TODO
+  // maybe we save data to query
+
+  // var readAll = function() {
+  $.ajax({
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+    type: 'GET',
+    data: {
+      q: query,
+      part: 'snippet',
+      maxResults: 8,
+      type: 'video',
+      videoEmbeddable: true,
+    },
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('DATA RETRIEVED');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('FAILED TO RETRIEVE DATA', data);
+    }
+  });
+  // };
+
+  // readAll((data) => {
+  //   query = data;
+  // }).call();
+
+  // readAll(function(data) {
+  //   query = data;
+  // }).call();
+
+
 };
 
 export default searchYouTube;
+
+
+//endpoint:
+//https://app-hrsei-api.herokuapp.com/api/recastly/videos
+
+/* example from chatterbox sprint (post)
+
+$.ajax({
+  // This is the url you should use to communicate with the API server.
+  url: 'https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/CAMPUS',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
+
+*/
